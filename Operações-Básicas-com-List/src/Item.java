6071d43938203db27e35bfaf59a44b00ae0873c7
Felipexvv <felipexvv@gmail.com>
2024-06-04 15:10:20 -0300

    Adicionando atividade CarrinhoDeCompras JAVA-DIO

---
+++ Operações-Básicas-com-List/src/Item.java
@@ -0,0 +1,24 @@
+public class Item {
+    String nome;
+    Double preco;
+    int quantidade;
+
+    public Item(String nome, double preco, int quantidade){
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    } 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+    
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+}
